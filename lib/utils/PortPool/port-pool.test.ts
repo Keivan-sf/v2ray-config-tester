d60c13a2d916f900f4ab3f9c311d6eb6cd24a234
Keivan-sf <keyvan0082@gmail.com>
2025-07-04 12:31:43 +0330

    Add port pool to run tasks concurrently

---
+++ lib/utils/PortPool/port-pool.test.ts
@@ -0,0 +1,25 @@
+import { PortPool } from "./port-pool";
+
+describe("Port pool", () => {
+  it("should normally return free ports", async () => {
+    const pool = new PortPool(4010, 4012);
+    const ports = [];
+    for (let i = 0; i < 3; i++) {
+      ports.push(await pool.getPort());
+    }
+    expect(ports).toStrictEqual([4010, 4011, 4012]);
+  });
+
+  it("should return free ports after another one is freed", async () => {
+    const pool = new PortPool(4010, 4012);
+    const ports = [];
+    for (let i = 0; i < 3; i++) {
+      ports.push(await pool.getPort());
+    }
+    setTimeout(() => {
+      pool.free(4010);
+    }, 100);
+    const another_port = await pool.getPort();
+    expect(another_port).toBe(4010);
+  });
+});
---
+++ lib/utils/PortPool/port-pool.ts
@@ -0,0 +1,37 @@
+export class PortPool {
+  private ports: { [key: number]: "free" | "busy" } = {};
+  private request_queue: ((port: number) => void)[] = [];
+  constructor(
+    private start: number,
+    private end: number,
+  ) {
+    for (let i = start; i <= end; i++) {
+      this.ports[i] = "free";
+    }
+    console.log("ports:", this.ports);
+  }
+
+  free(port: number) {
+    if (port > this.end || port < this.start)
+      throw new Error("port out of range");
+    if (this.request_queue.length > 0) {
+      const resolve = this.request_queue.shift()!;
+      resolve(port);
+    } else {
+      this.ports[port] = "free";
+    }
+  }
+
+  getPort(): Promise<number> {
+    return new Promise((resolve) => {
+      for (let port = this.start; port <= this.end; port++) {
+        if (this.ports[port] == "free") {
+          this.ports[port] = "busy";
+          resolve(port);
+          return;
+        }
+      }
+      this.request_queue.push(resolve);
+    });
+  }
+}
